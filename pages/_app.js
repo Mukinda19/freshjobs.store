26c7ede7d33aa760920c956ff52bd87aaf164929
Mukund Jagtap <admin@jjrsplacement.in>
2025-09-26 11:25:23 +0530

    Replace _app.js to include global Header

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/globals.css'
+// pages/_app.js
+import "../styles/globals.css";
+import Header from "../components/Header";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <main className="container mx-auto px-4 py-8">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
+
+export default MyApp;
