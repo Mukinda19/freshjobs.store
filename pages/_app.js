a9ad69a305ee1cf4d13181600f2ac1ceb02060fc
Mukund Jagtap <admin@jjrsplacement.in>
2025-09-27 16:20:51 +0530

    Fix single header + clear nav styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
-// pages/_app.js
-import '@/styles/globals.css';
-import Header from '@/components/Header';
+import '../styles/globals.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <main className="max-w-7xl mx-auto px-4 py-6">
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
+      <Footer />
     </>
-  );
+  )
 }
