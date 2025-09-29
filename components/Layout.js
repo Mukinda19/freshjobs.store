772a5816d96501b4052a9280ea9e60db273605e3
Mukund Jagtap <admin@jjrsplacement.in>
2025-09-29 11:36:17 +0530

    Add global Layout with Header & Fotter

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+// components/Layout.js
+import Header from './Header';
+import Footer from './Footer';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen bg-gray-50 p-4">
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+}
